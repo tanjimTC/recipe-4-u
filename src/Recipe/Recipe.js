b46cc5b4aff630fc4e524c0e1fb3a079542168fe
tanjimTC <tanjimtc74@gmail.com>
2020-04-30 01:13:20 +0600

    first commit

---
+++ src/Recipe/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//a60a9308
+//1f955083069a1c29e9664e87f4026b94
+//https://api.edamam.com/search?q=chicken&app_id=${a60a9308}&app_key=${1f955083069a1c29e9664e87f4026b94}
+const Recipe = () => {
+    return (
+        <div>
+            <h1>HEllo</h1>
+        </div>
+    );
+};
+
+export default Recipe;
