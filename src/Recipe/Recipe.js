14124a35f0acf73c9da2e33fef7103b468259a78
tanjimTC <tanjimtc74@gmail.com>
2020-04-30 04:22:02 +0600

    updated

--- src/Recipe/Recipe.js
+++ src/Recipe/Recipe.js
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import RecipeItems from "./RecipeItems";
+import 'tachyons';
+import './items.css'
 //a60a9308
 //1f955083069a1c29e9664e87f4026b94
 //https://api.edamam.com/search?q=chicken&app_id=${a60a9308}&app_key=${1f955083069a1c29e9664e87f4026b94}
 const Recipe = () => {
-    return (
-        <div>
-            <h1>HEllo</h1>
-        </div>
+  let ID = "a60a9308";
+  let KEY = "1f955083069a1c29e9664e87f4026b94";
+
+
+  const [recipe, setRecipe] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('banana')
+
+  useEffect(() => {
+    getRecipe();
+  }, [query]);
+
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+
+  const getSearch = (e)=>{
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
+
+
+  const getRecipe = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${ID}&app_key=${KEY}`
     );
+    const data = await response.json();
+    console.log(data);
+    setRecipe(data.hits);
+  };
+
+  return (
+    <div className="body">
+      <form onSubmit={getSearch} className='form'>
+        <input placeholder='search' className='input-field' type="text" value={search} onChange={handleChange} />
+        <button className='search-button'>Search</button>
+      </form>
+      <div className="recipes">
+      {recipe.map((x) => (
+        <RecipeItems
+          key={x.recipe.label}
+          image={x.recipe.image}
+          title={x.recipe.label}
+          cal={x.recipe.calories}
+          ingredients={x.recipe.ingredients}
+        />
+      ))}
+      </div> <br/> <br/>
+    </div> 
+  );
 };
 
 export default Recipe;
---
+++ src/Recipe/RecipeItems.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "tachyons";
+import "./items.css";
+
+const RecipeItems = ({ image, title, cal, ingredients }) => {
+  return (
+    <div className="card-body grow tc">
+        <h1 className="f4 tc">{title}</h1>
+        <hr className="mw3 bb bw1 b--black-10"></hr>
+        <ol className='list'>
+          {ingredients.map((x) => (
+            <li>👉 {x.text}</li>
+          ))}
+        </ol>
+        <p className='tc pt2 pb2'>total calories 🔥  {cal}</p>
+        <img
+          src={image}
+          alt=""
+          className="br-100 h4 b--black-05 pa2"
+        />
+    </div>
+  );
+};
+
+export default RecipeItems;
