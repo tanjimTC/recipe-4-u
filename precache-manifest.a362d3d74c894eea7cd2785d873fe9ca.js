dc88a5af8eec94969fa9b3ce946b925c98ab8468
tanjimTC <tanjimtc74@gmail.com>
2020-05-02 01:46:25 +0600

    Updates

---
+++ precache-manifest.a362d3d74c894eea7cd2785d873fe9ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf62a2f923c6acc15c8239decd6a99e",
+    "url": "/recipe-4-u/index.html"
+  },
+  {
+    "revision": "f92f4fdf1080fbcf9b7f",
+    "url": "/recipe-4-u/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "257babe339882fd32c3e",
+    "url": "/recipe-4-u/static/css/main.ec3e9636.chunk.css"
+  },
+  {
+    "revision": "f92f4fdf1080fbcf9b7f",
+    "url": "/recipe-4-u/static/js/2.a3ce786f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-4-u/static/js/2.a3ce786f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257babe339882fd32c3e",
+    "url": "/recipe-4-u/static/js/main.e99701a4.chunk.js"
+  },
+  {
+    "revision": "d961406add448914eab7",
+    "url": "/recipe-4-u/static/js/runtime-main.639d3116.js"
+  }
+]);
